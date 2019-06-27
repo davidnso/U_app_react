c796370374d1af6043a9be0b8886105749b4504b
davidnso <davidnsoesie1@gmail.com>
2019-06-27 00:32:56 -0400

    add test implementation to see if user message notifications are being retrieved from the native context

--- u-client/HomeView.tsx
+++ u-client/HomeView.tsx
@@ -1,16 +1,28 @@
 import React, {Component} from 'react'
-import {View, Text, Image, Button} from 'react-native'
+import Message from 'react-native-module-lib';
+import {View, Text, Image, Button, DeviceEventEmitter} from 'react-native'
 import NavBar from './components/nav-bar-component/NavBar';
 import StatsView from './components/stats-component/StatsView';
 import ContactView from './components/contact-view-component/ContactView';
 import ReminderDrawer from './components/reminder-drawer-component/ReminderDrawer'
+
 export default class HomeView extends Component{
+    componentDidMount(){
+        try{ 
+     Message.on('message', (data:any)=>{
+        console.log(JSON.stringify(data))
+     })
+    }
+     catch(err){
+        console.log(err)
+    }
+       
+    }
     render(){
+
         return(
         <View>
-            <NavBar/>
-            <StatsView/>
-            <ReminderDrawer/>
+            <Text>This is home</Text>
         </View>)
     }
 }
